b0dd7510c4641f587fa572b91e0d8b709b6f09dc
Varun Kumar Gupta <varunkumarg135@gmail.com>
2026-03-25 02:49:08 +0000

    edit resume link

--- src/data/personal-info.ts
+++ src/data/personal-info.ts
@@ -30,7 +30,7 @@ export const personalInfo: PersonalInfo = {
   ],
   heroLinks: [
     {
-      href: "https://drive.google.com/file/d/1nJW-7YqwYJg8KLV7y5E4y3UJhuEktBtB/view?usp=sharing",
+      href: "https://drive.google.com/file/d/1yto7aIEJZkPcc3ferCAwEzywDV6ub8KR/view?usp=sharing",
       label: "View Resume",
       variant: "primary"
     },
